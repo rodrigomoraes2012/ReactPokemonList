661b9438b2bdad4222588391e930865d37f04dff
Rodrigo Matos Moraes <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-01-01 19:43:54 -0300

    Updates

---
+++ precache-manifest.5b3434b6dfee876f31cb56f740c96931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799425d8b4f16992a88569f1f05fb546",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6be2a038d412bc3ceba4",
+    "url": "/monsters-rolodex/static/css/main.a59f9bdc.chunk.css"
+  },
+  {
+    "revision": "3e226d037caaae7579dc",
+    "url": "/monsters-rolodex/static/js/2.a6af99e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.a6af99e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6be2a038d412bc3ceba4",
+    "url": "/monsters-rolodex/static/js/main.935a8ca3.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
